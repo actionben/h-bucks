731c5790096c74cb2039905a9870ea8e0afb816b
Action Ben <ben@actionhank.co.uk>
2019-09-26 19:07:18 +0100

    Moved chores data into the controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,21 @@
 class WelcomeController < ApplicationController
   def index
+    @chores = [
+      {
+        name: 'Empty Dishwasher',
+        prize: 'HB 547',
+        status: 'Not Ready'
+      },
+      {
+        name: 'Load Dishwasher',
+        prize: 'HB 635',
+        status: 'Taken'
+      },
+      {
+        name: 'Wash Car',
+        prize: 'HB 475',
+        status: 'Ready'
+      }
+    ]
   end
 end
